723a720530cb7904215857d8c2d946add7b89b7e
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-11-11 13:39:00 -0800

    create API Call

--- src/App.js
+++ src/App.js
@@ -6,7 +6,26 @@ import axios from "axios";
 function App() {
   const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS)
 
-
+// API Call
+useEffect(() => {
+  axios
+  .get('https://opentdb.com/api.php?amount=10')
+  .then(res => {
+    setFlashcards(res.data.results.map((questionItem, index) => {
+      const answer = decodeString(questionItem.correct_answer)
+      const options = [
+        ...questionItem.incorrect_answers.map(a => decodeString(a)), 
+        answer
+      ]
+      return {
+        id: `${index}-${Date.now()}`,
+        question: decodeString(questionItem.question),
+        answer: answer,
+        options: options.sort(() => Math.random() - .5)
+      }
+    }))
+  })
+}, [])
 
   function decodeString(str) {
     const textArea = document.createElement('textarea')
