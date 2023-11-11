e115338248790a7a2c69568c3e508bbfdad0bd67
Vladimir Kratinov <vladimirkratinov@gmail.com>
2023-11-11 14:25:56 -0800

    connect API to header

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import "./app.css";
 import axios from "axios";
 
 function App() {
-  const [flashcards, setFlashcards] = useState(SAMPLE_FLASHCARDS);
+  const [flashcards, setFlashcards] = useState([]);
   const [categories, setCategories] = useState([]);
 
   const categoryEl = useRef();
   const amountEl = useRef();
-  
 
   useEffect(() => {
     axios.get("https://opentdb.com/api_category.php").then((res) => {
@@ -19,7 +18,24 @@ function App() {
 
   // API Call
   useEffect(() => {
-    axios.get("https://opentdb.com/api.php?amount=10").then((res) => {
+    
+  }, []);
+
+  function decodeString(str) {
+    const textArea = document.createElement("textarea");
+    textArea.innerHTML = str;
+    return textArea.value;
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    axios.get("https://opentdb.com/api.php", {
+      params: {
+        amount: amountEl.current.value,
+        category: categoryEl.current.value
+      }
+    })
+    .then((res) => {
       setFlashcards(
         res.data.results.map((questionItem, index) => {
           const answer = decodeString(questionItem.correct_answer);
@@ -36,16 +52,6 @@ function App() {
         })
       );
     });
-  }, []);
-
-  function decodeString(str) {
-    const textArea = document.createElement("textarea");
-    textArea.innerHTML = str;
-    return textArea.value;
-  }
-
-  function handleSubmit(e) {
-    e.preventDefault();
   }
 
   return (
@@ -65,7 +71,14 @@ function App() {
         </div>
         <div className="form-group">
           <label htmlFor="amount">Number Of Questions</label>
-          <input type="number" id="amount" min="1" step="1" defaultValue={10} ref={amountEl} />
+          <input
+            type="number"
+            id="amount"
+            min="1"
+            step="1"
+            defaultValue={10}
+            ref={amountEl}
+          />
         </div>
         <div className="form-group">
           <button className="btn">Generate</button>
@@ -77,20 +90,5 @@ function App() {
     </>
   );
 }
-
-const SAMPLE_FLASHCARDS = [
-  {
-    id: 1,
-    question: "What is 2 + 2?",
-    answer: "4",
-    options: ["2", "3", "4", "5"],
-  },
-  {
-    id: 2,
-    question: "Question 2?",
-    answer: "Answer 1",
-    options: ["Answer 1", "Answer 2", "Answer 3", "Answer 4"],
-  },
-];
 
 export default App;
